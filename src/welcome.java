fd69a1e3293ae5ce80980ea939a8012b1cca98cc
ssk <shivam.kalbande23@gmail.com>
2021-11-11 14:34:00 +0530

    first commit

---
+++ src/welcome.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Java:");
+	//while(true)
+	//{
+		Scanner ip1= new Scanner(System.in);
+		System.out.println("Enter your name :");
+		String name = ip1.next();
+		
+		System.out.println(name);
+		System.out.println(name.length());
+		//String s=name;
+		
+		/*if ("shivamkalbande"==s){
+			System.out.println("Verified");
+			
+			break;
+			}else{
+			System.out.println("Not Verified"); 		
+			}*/
+	ip1.close();
+		return ;}
+
+}
