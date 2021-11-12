3ac4b2eaee7c4bf3bd08d1810322bd88c7bb38a6
ssk232017 <94100131+ssk232017@users.noreply.github.com>
2021-11-12 10:11:53 +0530

    factorial

---
+++ src/fact.java
@@ -0,0 +1,12 @@
+class fact{
+      int n;
+      int fact=0;
+      public void fact(){
+          for(int i=1; i<=n; i++){
+            fact=fact+(n*i);
+        
+          }
+        System.out.println("Factorial of "+n+" is "+fact);
+      }
+    }
+}
